1bfb462cee86f5be420fcc1ca28ee35359f9b69c
Dagmawilencho <86290412+Dagmawilencho@users.noreply.github.com>
2021-07-22 23:30:41 +0300

    Update scraper.py
    D

--- scraper.py
+++ scraper.py
@@ -100,4 +100,4 @@ with open("members.csv","w",encoding='UTF-8') as f:
             last_name= ""
         name= (first_name + ' ' + last_name).strip()
         writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
-print(gr+'[+] Members scraped successfully. Subscribe Termux Professor Youtube Channel For Add Members')
+print(gr+'[+] Members scraped successfully. Subscribe Dagi Tech Youtube Channel For Add Members')
